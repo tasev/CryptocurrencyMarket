0b67c5d9b8429cf45f25bbd244cc33c39b169c9d
tasev <tase.tasev@gmail.com>
2017-12-08 01:28:03 +0100

    CryptoData Model created

---
+++ app/src/main/java/market/android/cryptocurrency/com/cryptocurrencymarket/data/CryptoData.java
@@ -0,0 +1,41 @@
+package market.android.cryptocurrency.com.cryptocurrencymarket.data;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Created by tasev on 12/8/17.
+ */
+
+public class CryptoData implements Serializable {
+
+    public String id;
+    public String name;
+    public String symbol;
+    public Integer rank;
+    public double price_usd;
+    public double price_btc;
+    @SerializedName("24h_volume_usd")
+    public double h_volume_usd;
+    public String market_cap_usd;
+    public double available_supply;
+    public double total_supply;
+    public double max_supply;
+    public double percent_change_1h;
+    public double percent_change_24h;
+    public double percent_change_7d;
+    public String last_updated;
+
+    //EUR
+    public double price_eur;
+    @SerializedName("24h_volume_eur")
+    public double h_volume_eur;
+    public String market_cap_eur;
+
+    //CNY
+    public double price_cny;
+    @SerializedName("24h_volume_cny")
+    public double h_volume_cny;
+    public String market_cap_cny;
+}
